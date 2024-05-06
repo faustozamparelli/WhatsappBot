1359bfb7c6069080e01c2f7607f5b4b5801a49ca
faustozamparelli <fausto.zamparelli@gmail.com>
2024-05-07 01:06:58 +0200

    progress

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-// const { Client } = require('whatsapp-web.js');
-// const qrcode = require('qrcode-terminal');
-//
-// const client = new Client({
-//   webVersionCache: {
-//     type: "remote",
-//     remotePath:
-//       "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-//   },
-// });
-//
-// client.on('qr', qr => {
-//     qrcode.generate(qr, {small: true});
-// });
-//
-// client.on('ready', () => {
-//     console.log('Client is ready!');
-//
-// client.initialize();
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const fs = require("fs");
 
-const hour = new Date().getHours();
-const min = new Date().getMinutes();
+const client = new Client({
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
+});
 
-console.log(hour + ":" + min)
-console.log(random)
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
 
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
 
+client.initialize();
 
+client.on("message_create", (message) => {
+  if (
+    Contact.name === "Lus" &&
+    message.body.toLowerCase().startsWith("buongiorno")
+  ) {
+    fs.readFile("./morning_response.csv", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
+      const responses = data.split("\n");
+
+      const randomResponse =
+        responses[Math.floor(Math.random() * responses.length)];
+
+      const delay = Math.floor(Math.random() * 40 * 60 * 1000);
+
+      setTimeout(() => {
+        client.sendMessage(message.from, randomResponse);
+      }, delay);
+    });
+  }
+});
