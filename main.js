f5abadd934188de72a5df2e598746193149bcb2d
faustozamparelli <fausto.zamparelli@gmail.com>
2024-05-27 09:55:07 +0200

    working

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+"use strict";
 const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-const fs = require("fs");
+const Groq = require("groq-sdk");
 
 const client = new Client({
   webVersionCache: {
@@ -8,6 +9,10 @@ const client = new Client({
     remotePath:
       "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
   },
+});
+
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
 });
 
 client.on("qr", (qr) => {
@@ -20,28 +25,46 @@ client.on("ready", () => {
 
 client.initialize();
 
-client.on("message_create", (message) => {
-  if (
-    message.from === "393801426695@c.us" &&
-    message.body.toLowerCase().startsWith("buongiorno")
-  ) {
-    fs.readFile("./morning_response.csv", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
+const contacts = {
+  mamma: "393476522538@c.us",
+  papa: "393488573651@c.us",
+  helena: "393770999735@c.us",
+};
 
-      const responses = data.split("\n");
+const contactName = process.argv[2];
+const contactId = contacts[contactName];
 
-      const randomResponse =
-        responses[Math.floor(Math.random() * responses.length)];
-      //
-      // const delay = Math.floor(Math.random() * 50 * 60 * 1000);
-      //
-      // setTimeout(() => {
-      //   client.sendMessage(message.from, randomResponse);
-      // }, delay);
-      client.sendMessage(message.from, randomResponse);
-    });
+if (!contactId) {
+  console.error("Unknown contact name:", contactName);
+  process.exit(1);
+}
+
+async function getGroqChatCompletion(messages) {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are texting to one of my contacts and you will respond as if you were me. The messages will be in italian and i usually don't use capital letters or punctuations except periods",
+      },
+      ...messages.map((message) => ({
+        role: "user",
+        content: message,
+      })),
+    ],
+    model: "llama3-70b-8192",
+  });
+}
+
+client.on("message_create", async (message) => {
+  if (message.from === contactId) {
+    const chatCompletion = await getGroqChatCompletion([message.body]);
+    const responses =
+      chatCompletion.choices[0]?.message?.content.split(". ") || [];
+    for (const response of responses) {
+      // const delay = Math.random() * (20 * 60 - 2) + 2; // delay between 2 seconds and 20 minutes
+      // await new Promise((resolve) => setTimeout(resolve, delay * 1000));
+      message.reply(response);
+    }
   }
 });
